68205d3ef4cdb1b3f284227312bbb7b5c72634d4
captainwasi123 <captain.wasi@gmail.com>
2023-12-21 21:43:27 +0400

    seeder update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,25 +20,25 @@ class DatabaseSeeder extends Seeder
     {
         
         //Dummy User
-            //User::create(array('name' => 'Test User', 'email' => 'abc@gmail.com', 'gender' => 'Male', 'type' => '1', 'password' => bcrypt('abc123'), 'is_active' => '1', 'created_by' => '0'));
+            User::create(array('name' => 'Test User', 'email' => 'abc@gmail.com', 'gender' => 'Male', 'type' => '1', 'password' => bcrypt('abc123'), 'is_active' => '1', 'created_by' => '0'));
 
         //Default Country
-            Countries::create(array('name' => 'United Arab Emirates'));
+            //Countries::create(array('name' => 'United Arab Emirates'));
 
 
         //Default State
-            States::create(array('name' => 'Dubai', 'country_id' => '1'));
+            //States::create(array('name' => 'Dubai', 'country_id' => '1'));
 
 
         //Default Zone
-            Zones::create(array('name' => 'Al Barsha', 'state_id' => '1'));
-            Zones::create(array('name' => 'Al Barsha First', 'state_id' => '1'));
-            Zones::create(array('name' => 'Al Barsha Second', 'state_id' => '1'));
+            //Zones::create(array('name' => 'Al Barsha', 'state_id' => '1'));
+            //Zones::create(array('name' => 'Al Barsha First', 'state_id' => '1'));
+            //Zones::create(array('name' => 'Al Barsha Second', 'state_id' => '1'));
 
 
         //Customer Type
-            CustomerType::create(array('name' => 'Pharmacy'));
-            CustomerType::create(array('name' => 'Baqala'));
+            //CustomerType::create(array('name' => 'Pharmacy'));
+            //CustomerType::create(array('name' => 'Baqala'));
 
  
         $this->command->info('User table seeded!');
